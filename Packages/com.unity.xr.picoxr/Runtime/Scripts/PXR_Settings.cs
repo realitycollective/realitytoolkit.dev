80996afddfc70585f3a40b73b58263bf91a2228c
Simon Jackson <darkside@zenithmoon.com>
2022-02-11 15:37:57 +0000

    Added Pico and WindowsXR packages. Updated Assembly defs and infos to RealityToolkit

---
+++ Packages/com.unity.xr.picoxr/Runtime/Scripts/PXR_Settings.cs
@@ -0,0 +1,62 @@
+﻿/************************************************************************************
+ 【PXR SDK】
+ Copyright 2015-2020 Pico Technology Co., Ltd. All Rights Reserved.
+
+************************************************************************************/
+
+using System;
+using UnityEngine;
+using UnityEngine.XR.Management;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Unity.XR.PXR
+{
+    [Serializable]
+    [XRConfigurationData("PicoXR", "Unity.XR.PXR.Settings")]
+    public class PXR_Settings : ScriptableObject
+    {
+        public enum StereoRenderingModeAndroid
+        {
+            /// <summary>
+            /// Unity makes two passes across the scene graph, each one entirely independent of the other. 
+            /// Each pass has its own eye matrices and render target. Unity draws everything twice, which includes setting the graphics state for each pass. 
+            /// This is a slow and simple rendering method which doesn't require any special modification to shaders.
+            /// </summary>
+            MultiPass = 0,
+             /// <summary>
+            /// Unity uses a single texture array with two elements. 
+            /// Multiview is very similar to Single Pass Instanced; however, the graphics driver converts each call into an instanced draw call so it requires less work on Unity's side. 
+            /// As with Single Pass Instanced, shaders need to be aware of the Multiview setting. Unity's shader macros handle the situation.
+            /// </summary>
+            Multiview = 1
+        }
+
+        public enum SystemDisplayFrequency
+        {
+            Default,
+            RefreshRate72,
+            RefreshRate90,
+        }
+
+        [SerializeField, Tooltip("Set the Stereo Rendering Method")]
+        public StereoRenderingModeAndroid stereoRenderingModeAndroid;
+        
+        [SerializeField, Tooltip("Set the system display refresh rates")]
+        public SystemDisplayFrequency systemDisplayFrequency;
+
+        public ushort GetStereoRenderingMode()
+        {
+            return (ushort)stereoRenderingModeAndroid;
+        }
+
+#if !UNITY_EDITOR
+		public static PXR_Settings settings;
+		public void Awake()
+		{
+            settings = this;
+		}
+#endif
+    }
+}
