aa62022ce68908ba39ff5f2b5870111e019675ab
Dino Fejzagić <dino.f@live.de>
2022-07-11 21:37:27 +0200

    Udpate MS XR Plugin

--- Assets/DebugInput.cs
+++ Assets/DebugInput.cs
@@ -3,8 +3,18 @@ using RealityToolkit.Interfaces.InputSystem.Handlers;
 using RealityToolkit.Services.InputSystem.Listeners;
 using UnityEngine;
 
-public class DebugInput : InputSystemGlobalListener, IMixedRealityInputHandler
+public class DebugInput : InputSystemGlobalListener, IMixedRealityInputHandler, IMixedRealityInputHandler<float>
 {
+    public void OnInputChanged(InputEventData<float> eventData)
+    {
+        if (!InputSystem.TryGetController(eventData.InputSource, out var controller))
+        {
+            return;
+        }
+
+        Debug.Log($"{nameof(OnInputChanged)} | {controller.GetType().Name} | {eventData.MixedRealityInputAction.Description} | {eventData.InputData}");
+    }
+
     public void OnInputDown(InputEventData eventData)
     {
         if (!InputSystem.TryGetController(eventData.InputSource, out var controller))
--- Packages/com.microsoft.mixedreality.openxr/Editor/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Reflection;
-
-[assembly: AssemblyVersion("1.2.1")]
