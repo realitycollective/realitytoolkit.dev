f5d350555aae657bb44a400ef154005f00e3eb13
Dino Fejzagić <dino.f@live.de>
2023-08-17 22:11:12 +0200

    Add sample force push action

---
+++ Assets/Actions/ForcePushAction.cs
@@ -0,0 +1,39 @@
+using RealityToolkit.Input.Events;
+using RealityToolkit.Input.InteractionActions;
+using RealityToolkit.Input.Interactors;
+using UnityEngine;
+
+public class ForcePushAction : BaseInteractionAction
+{
+    [SerializeField, Tooltip("The power of the force push executed on the object.")]
+    private float forcePower = 10f;
+
+    private new Rigidbody rigidbody;
+
+    /// <inheritdoc/>
+    protected override void Awake()
+    {
+        base.Awake();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <inheritdoc/>
+    public override void OnLastGrabExited(InteractionExitEventArgs eventArgs)
+    {
+        base.OnLastGrabExited(eventArgs);
+
+        // This action only works with controller based interactors.
+        if (eventArgs.Interactor is IControllerInteractor controllerInteractor)
+        {
+            // We determine the direction of our force push using the interactor's position in the scene
+            // and the interactable's position and normalize it.
+            var forceDirection = (transform.position - controllerInteractor.GameObject.transform.position).normalized;
+
+            // Scale by our configured force power.
+            forceDirection = forcePower * forceDirection;
+
+            // Finally add the force push to the rigidboy.
+            rigidbody.AddForce(forceDirection, ForceMode.Impulse);
+        }
+    }
+}
