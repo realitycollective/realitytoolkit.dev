1f6bae21d72272eedd6638d77605908f21ab41a8
Dino Fejzagić <dino.f@live.de>
2024-03-31 19:58:01 +0200

    x

--- Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/RotateLeverBehaviour.cs
+++ Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/RotateLeverBehaviour.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using RealityToolkit.Input.InteractionBehaviours;
+using UnityEngine;
 
 namespace RealityToolkit.Core.Samples.Interactions
 {
-    public class RotateLeverBehaviour : MonoBehaviour
+    [HelpURL("https://www.realitytoolkit.io/docs/interactions/interaction-behaviours/default-behaviours/rotate-lever-behaviour")]
+    public class RotateLeverBehaviour : BaseInteractionBehaviour
     {
         [SerializeField]
         [Tooltip("The pivot transform determines the coordinate space to rotate in.")]
