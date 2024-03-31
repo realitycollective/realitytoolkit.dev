244a83404bb0d9f016d0a5b2fe124b6ba4cbd3bf
Dino Fejzagić <dino.f@live.de>
2024-03-31 11:55:25 +0200

    Current state of first interactions sample

---
+++ Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/TranslateLeverBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using RealityToolkit.Input.Events;
+using RealityToolkit.Input.InteractionBehaviours;
+using RealityToolkit.Input.Interactors;
+using UnityEngine;
+
+namespace RealityToolkit.Core.Samples.Interactions
+{
+    public class TranslateLeverBehaviour : BaseInteractionBehaviour
+    {
+        [SerializeField]
+        private float minMaxThrottleOffset = .05f;
+
+        [SerializeField]
+        [Tooltip("The pivot transform for the lever to rotate at.")]
+        private Transform pivot = null;
+
+        private IControllerInteractor currentInteractor;
+        private Vector3 previousInteractorPosition;
+
+        /// <inheritdoc/>
+        protected override void Update()
+        {
+            if (currentInteractor == null)
+            {
+                return;
+            }
+
+            var currentInteractorPosition = pivot.InverseTransformPoint(currentInteractor.GameObject.transform.position);
+            var deltaZ = currentInteractorPosition.z - previousInteractorPosition.z;
+
+            var leverPosition = transform.localPosition;
+            var leverOffset = Mathf.Clamp(leverPosition.z + deltaZ, -minMaxThrottleOffset, minMaxThrottleOffset);
+
+            leverPosition.z = leverOffset;
+            transform.localPosition = leverPosition;
+
+            previousInteractorPosition = currentInteractorPosition;
+        }
+
+        /// <inheritdoc/>
+        protected override void OnFirstGrabEntered(InteractionEventArgs eventArgs)
+        {
+            if (eventArgs.Interactor is not IControllerInteractor controllerInteractor)
+            {
+                return;
+            }
+
+            currentInteractor = controllerInteractor;
+            previousInteractorPosition = pivot.InverseTransformPoint(currentInteractor.GameObject.transform.position);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnLastGrabExited(InteractionExitEventArgs eventArgs)
+        {
+            currentInteractor = null;
+        }
+    }
+}
