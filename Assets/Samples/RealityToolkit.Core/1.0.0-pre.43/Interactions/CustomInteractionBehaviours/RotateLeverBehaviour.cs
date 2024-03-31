59f87e58f0b89741d0e646835e70a76bb4ddfa5f
Dino Fejzagić <dino.f@live.de>
2024-03-31 12:27:37 +0200

    Add support for multiple axes to TranslateLeverBehaviour

---
+++ Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/RotateLeverBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RealityToolkit.Core.Samples.Interactions
+{
+    public class RotateLeverBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The pivot transform determines the coordinate space to rotate in.")]
+        private Transform pivot = null;
+    }
+}
--- Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/TranslateLeverBehaviour.cs
+++ Assets/Samples/RealityToolkit.Core/1.0.0-pre.43/Interactions/CustomInteractionBehaviours/TranslateLeverBehaviour.cs
@@ -7,11 +7,13 @@ namespace RealityToolkit.Core.Samples.Interactions
 {
     public class TranslateLeverBehaviour : BaseInteractionBehaviour
     {
-        [SerializeField]
-        private float minMaxThrottleOffset = .05f;
+        [SerializeField, Tooltip("The axes to translate upon interaction.")]
+        private SnapAxis axes = SnapAxis.Z;
 
-        [SerializeField]
-        [Tooltip("The pivot transform for the lever to rotate at.")]
+        [SerializeField, Tooltip("Translation limits per axis in each direction.")]
+        private Vector3 translateThresholds = new Vector3(0f, 0f, .1f);
+
+        [SerializeField, Tooltip("The pivot transform determines the coordinate space to translate in.")]
         private Transform pivot = null;
 
         private IControllerInteractor currentInteractor;
@@ -26,12 +28,27 @@ namespace RealityToolkit.Core.Samples.Interactions
             }
 
             var currentInteractorPosition = pivot.InverseTransformPoint(currentInteractor.GameObject.transform.position);
-            var deltaZ = currentInteractorPosition.z - previousInteractorPosition.z;
+            var delta = Vector3.zero;
+
+            if ((axes & SnapAxis.X) > 0)
+            {
+                delta.x = currentInteractorPosition.x - previousInteractorPosition.x;
+            }
+
+            if ((axes & SnapAxis.Y) > 0)
+            {
+                delta.y = currentInteractorPosition.y - previousInteractorPosition.y;
+            }
+
+            if ((axes & SnapAxis.Z) > 0)
+            {
+                delta.z = currentInteractorPosition.z - previousInteractorPosition.z;
+            }
 
             var leverPosition = transform.localPosition;
-            var leverOffset = Mathf.Clamp(leverPosition.z + deltaZ, -minMaxThrottleOffset, minMaxThrottleOffset);
-
-            leverPosition.z = leverOffset;
+            leverPosition.x = Mathf.Clamp(leverPosition.x + delta.x, -translateThresholds.x, translateThresholds.x);
+            leverPosition.y = Mathf.Clamp(leverPosition.y + delta.y, -translateThresholds.y, translateThresholds.y);
+            leverPosition.z = Mathf.Clamp(leverPosition.z + delta.z, -translateThresholds.z, translateThresholds.z);
             transform.localPosition = leverPosition;
 
             previousInteractorPosition = currentInteractorPosition;
