08c2319e8a05f903796dd1cc8ec5d20831f35748
Dino Fejzagić <dino.f@live.de>
2024-04-22 20:52:49 +0200

    x

---
+++ Assets/Samples/RealityToolkit.Samples/1.0.0-pre.0/SampleProject/ISampleProjectService.cs
@@ -0,0 +1,6 @@
+﻿using RealityCollective.ServiceFramework.Interfaces;
+
+namespace RealityToolkit.Samples.SampleProject
+{
+    public interface ISampleProjectService : IService { }
+}
---
+++ Assets/Samples/RealityToolkit.Samples/1.0.0-pre.0/SampleProject/SampleProjectService.cs
@@ -0,0 +1,41 @@
+﻿using RealityCollective.ServiceFramework.Definitions;
+using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.Locomotion;
+using UnityEngine;
+
+namespace RealityToolkit.Samples.SampleProject
+{
+    [System.Runtime.InteropServices.Guid("a7e7f7a9-fd59-4589-a501-080dd2d97afd")]
+    public class SampleProjectService : BaseServiceWithConstructor, ISampleProjectService
+    {
+        public SampleProjectService(string name, uint priority, BaseProfile profile)
+            : base(name, priority)
+        {
+
+        }
+
+        private ILocomotionService locomotionService;
+
+        /// <inheritdoc/>
+        public override void Initialize()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            locomotionService = ServiceManager.Instance.GetService<ILocomotionService>();
+        }
+
+        /// <inheritdoc/>
+        public override void Start()
+        {
+            InitialzeSampleProjectStartState();
+        }
+
+        private void InitialzeSampleProjectStartState()
+        {
+
+        }
+    }
+}
