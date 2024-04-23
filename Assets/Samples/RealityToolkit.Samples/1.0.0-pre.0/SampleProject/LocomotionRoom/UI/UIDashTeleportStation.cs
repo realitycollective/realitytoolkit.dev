7220a7b34a8b8cd957d633bb4a22fcd6cd51e862
Dino Fejzagić <dino.f@live.de>
2024-04-23 10:15:08 +0200

    x

---
+++ Assets/Samples/RealityToolkit.Samples/1.0.0-pre.0/SampleProject/LocomotionRoom/UI/UIDashTeleportStation.cs
@@ -0,0 +1,15 @@
+﻿using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.Locomotion;
+using RealityToolkit.Locomotion.Teleportation;
+using UnityEngine;
+
+namespace RealityToolkit.Samples.SampleProject.LocomotionRoom.UI
+{
+    public class UIDashTeleportStation : MonoBehaviour
+    {
+        public void EnabhleDashTeleport()
+        {
+            ServiceManager.Instance.GetService<ILocomotionService>().EnableLocomotionProvider(typeof(DashTeleportLocomotionProvider));
+        }
+    }
+}
---
+++ Assets/Samples/RealityToolkit.Samples/1.0.0-pre.0/SampleProject/LocomotionRoom/UI/UIInstantTeleportStation.cs
@@ -0,0 +1,15 @@
+﻿using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.Locomotion;
+using RealityToolkit.Locomotion.Teleportation;
+using UnityEngine;
+
+namespace RealityToolkit.Samples.SampleProject.LocomotionRoom.UI
+{
+    public class UIInstantTeleportStation : MonoBehaviour
+    {
+        public void EnabhleInstantTeleport()
+        {
+            ServiceManager.Instance.GetService<ILocomotionService>().EnableLocomotionProvider(typeof(InstantTeleportLocomotionProvider));
+        }
+    }
+}
