8ed667bbfd9be3de8b1d98f352827accfbcecae2
Dino Fejzagić <dino.f@live.de>
2024-05-11 23:57:54 +0200

    Working on teleportation room

---
+++ Assets/Samples/RealityToolkit.Samples/1.0.0-pre.0/SampleProject/Quests/BlinkTeleportActivator.cs
@@ -0,0 +1,15 @@
+﻿using RealityCollective.ServiceFramework.Services;
+using RealityToolkit.Locomotion;
+using RealityToolkit.Locomotion.Teleportation;
+using UnityEngine;
+
+namespace RealityToolkit.Samples.SampleProject.Quests
+{
+    public class BlinkTeleportActivator : MonoBehaviour
+    {
+        public void Activate()
+        {
+            ServiceManager.Instance.GetService<ILocomotionService>().EnableLocomotionProvider(typeof(BlinkTeleportLocomotionProvider));
+        }
+    }
+}
