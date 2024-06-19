6b9c00285fe165c80e55c164057b3bd717e2b00e
Dino Fejzagić <dino.f@live.de>
2024-06-19 20:18:02 +0200

    x

---
+++ Assets/RealityToolkit.Dev/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    private Transform target;
+
+    private void Awake()
+    {
+        target = transform;
+    }
+
+    private void Update()
+    {
+        var position = target.position;
+        position.z += Time.deltaTime * speed;
+        target.position = position;
+    }
+}
